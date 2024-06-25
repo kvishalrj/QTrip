c2090584145d538b32da63b7ace75719ab4bc748
Vishal Kumar <kvishalrj2020@gmail.com>
2024-06-25 19:56:35 +0530

    sold out banner added

--- adventure/views.py
+++ adventure/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.views import View
+from booking.models import Reservation
 
 class AdventurePageView(View):
     def get(self, request):
@@ -7,4 +8,14 @@ class AdventurePageView(View):
     
 class AdventureDetailPageView(View):
     def get(self, request):
-        return render(request, 'adventure/adventure_detail.html')
+        reservations = Reservation.objects.all()
+        adventure_ids = [reservation.adventure_id for reservation in reservations]
+        current_adventure_id = request.GET.get('adventure')
+        print(current_adventure_id)
+        print(adventure_ids)
+        is_adventure_in_list = current_adventure_id in adventure_ids
+        print(is_adventure_in_list)
+        context = {
+            'is_adventure_in_list': is_adventure_in_list
+        }
+        return render(request, 'adventure/adventure_detail.html', context)
