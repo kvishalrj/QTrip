97facace3b3d8a3438fcbdf705a26bdcbd511f44
Vishal Kumar <kvishalrj2020@gmail.com>
2024-06-26 13:20:04 +0530

    deployment start

--- build_files.sh
+++ build_files.sh
@@ -2,5 +2,5 @@
 pip install -r requirements.txt
 
 # make migrations
-python3.11 manage.py migrate 
-python3.11 manage.py collectstatic
+python3.9 manage.py migrate 
+python3.9 manage.py collectstatic
