f62c0f0caf2776010fffda7efd8455609f796825
Vishal Kumar <kvishalrj2020@gmail.com>
2024-06-27 12:30:42 +0530

    second commit

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Install Python and pip
+apt-get update && apt-get install -y python3 python3-pip
+
+# Install project dependencies
+pip install -r requirements.txt
+
 pip install -r requirements.txt
 
 # make migrations
