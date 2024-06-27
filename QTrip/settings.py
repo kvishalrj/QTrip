f19fc8b31a9ccd40b8e9186bb8039b9c0f7fa63b
Vishal Kumar <kvishalrj2020@gmail.com>
2024-06-27 15:33:11 +0530

    final commit

--- QTrip/settings.py
+++ QTrip/settings.py
@@ -137,8 +137,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
-
-
+STATIC_ROOT = BASE_DIR / "staticfiles"  # Example static root
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
