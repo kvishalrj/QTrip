5a4baee736364a0a6a75a1302ce6ca70334db80f
Vishal Kumar <kvishalrj2020@gmail.com>
2024-06-27 12:42:02 +0530

    json updated

--- build_files.sh
+++ build_files.sh
@@ -1,18 +1,6 @@
-#!/bin/bash
+# build_files.sh
+pip install -r requirements.txt
 
-# Update package lists
-sudo apt-get update
-
-# Install Python and pip
-sudo apt-get install -y python3.9 python3.9-venv python3.9-dev
-
-# Ensure pip is installed and upgrade to the latest version
-sudo apt-get install -y python3-pip
-python3.9 -m pip install --upgrade pip
-
-# Install project dependencies
-python3.9 -m pip install -r requirements.txt
-
-# Make migrations and collect static files
-python3.9 manage.py migrate
-python3.9 manage.py collectstatic --noinput
+# make migrations
+python3.9 manage.py migrate 
+python3.9 manage.py collectstatic
