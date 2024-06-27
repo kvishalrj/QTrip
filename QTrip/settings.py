59d5d2b9da7a7534f6ed6052ac94d5fa7ad9c1bc
Vishal Kumar <kvishalrj2020@gmail.com>
2024-06-27 12:03:50 +0530

    static updated

--- QTrip/settings.py
+++ QTrip/settings.py
@@ -133,9 +133,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
--- build_files.sh
+++ build_files.sh
@@ -1,48 +1,7 @@
 #!/bin/bash
 
-# Step 1: Download and install Python
-PYTHON_VERSION="3.9.6"
-PYTHON_TAR="Python-${PYTHON_VERSION}.tgz"
-PYTHON_DIR="Python-${PYTHON_VERSION}"
-
-# Download Python source
-curl -O https://www.python.org/ftp/python/${PYTHON_VERSION}/${PYTHON_TAR}
-
-# Extract the tarball
-tar xzf ${PYTHON_TAR}
-
-# Navigate to the Python directory
-cd ${PYTHON_DIR}
-
-# Configure and install Python locally
-./configure --prefix=$HOME/python
-make
-make install
-
-# Go back to the original directory
-cd ..
-
-# Update PATH to use the newly installed Python
-export PATH=$HOME/python/bin:$PATH
-
-# Verify Python installation
-python --version
-
-# Step 2: Install pip
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-python get-pip.py --user
-
-# Ensure pip is in PATH
-export PATH=$HOME/.local/bin:$PATH
-
-# Verify pip installation
-pip --version
-
-# Step 3: Install dependencies
 pip install -r requirements.txt
 
-# Step 4: Collect static files
-python manage.py collectstatic --noinput
-
-# Step 5: Apply database migrations
-python manage.py migrate
+# make migrations
+python3.9 manage.py migrate 
+python3.9 manage.py collectstatic
