0c1c17db9ba123ee9bfe20dbb884bf439f56dc07
Vishal Kumar <kvishalrj2020@gmail.com>
2024-06-27 13:11:33 +0530

    static updated

--- adventure/views.py
+++ adventure/views.py
@@ -11,10 +11,7 @@ class AdventureDetailPageView(View):
         reservations = Reservation.objects.all()
         adventure_ids = [reservation.adventure_id for reservation in reservations]
         current_adventure_id = request.GET.get('adventure')
-        print(current_adventure_id)
-        print(adventure_ids)
         is_adventure_in_list = current_adventure_id in adventure_ids
-        print(is_adventure_in_list)
         context = {
             'is_adventure_in_list': is_adventure_in_list
         }
